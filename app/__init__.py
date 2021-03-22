9eeb6f154d6950051832b5f461a432fe864cd15b
alex-pezzati <aapezzati@gmail.com>
2021-03-22 17:44:53 -0400

    aws implemented; testing continues

--- app/__init__.py
+++ app/__init__.py
@@ -9,6 +9,9 @@ from .models import db, User
 from .api.user_routes import user_routes
 from .api.auth_routes import auth_routes
 
+# testing aws
+from .api.aws_test import image_routes
+
 from .seeds import seed_commands
 
 from .config import Config
@@ -31,6 +34,7 @@ app.cli.add_command(seed_commands)
 app.config.from_object(Config)
 app.register_blueprint(user_routes, url_prefix='/api/users')
 app.register_blueprint(auth_routes, url_prefix='/api/auth')
+app.register_blueprint(auth_routes, url_prefix='/api/images')
 db.init_app(app)
 Migrate(app, db)
 
@@ -39,7 +43,7 @@ CORS(app)
 
 # Since we are deploying with Docker and Flask,
 # we won't be using a buildpack when we deploy to Heroku.
-# Therefore, we need to make sure that in production any 
+# Therefore, we need to make sure that in production any
 # request made over http is redirected to https.
 # Well.........
 
