64dc7f8c3404e2999ab7f35e42f3d4ab1fd4ec4e
raymondmay95 <raymond.may.95@outlook.com>
2021-03-23 07:50:11 -1000

    added front end form at /song route. passing to alex to check aws works

--- react-app/src/components/AWS.js
+++ react-app/src/components/AWS.js
@@ -1,56 +1,85 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import { useHistory } from "react-router-dom";
 
+export const UploadPicture = () => {
+  const history = useHistory(); // so that we can redirect after the image upload is successful
+  const [image, setImage] = useState(null);
+  const [imageLoading, setImageLoading] = useState(false);
 
-const UploadPicture = () => {
-    const history = useHistory(); // so that we can redirect after the image upload is successful
-    const [image, setImage] = useState(null);
-    const [imageLoading, setImageLoading] = useState(false);
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const formData = new FormData();
+    formData.append("image", image);
 
+    // aws uploads can be a bit slow—displaying
+    // some sort of loading message is a good idea
+    setImageLoading(true);
 
-    const handleSubmit = async (e) => {
-        e.preventDefault();
-        const formData = new FormData();
-        formData.append("image", image);
-
-        // aws uploads can be a bit slow—displaying
-        // some sort of loading message is a good idea
-        setImageLoading(true);
-
-        const res = await fetch('/api/images', {
-            method: "POST",
-            body: formData,
-        });
-        if (res.ok) {
-            await res.json();
-            setImageLoading(false);
-            history.push("/images");
-        }
-        else {
-            setImageLoading(false);
-            // a real app would probably use more advanced
-            // error handling
-            console.log("error");
-        }
+    const res = await fetch("/api/images", {
+      method: "POST",
+      body: formData,
+    });
+    if (res.ok) {
+      await res.json();
+      setImageLoading(false);
+      history.push("/images");
+    } else {
+      setImageLoading(false);
+      // a real app would probably use more advanced
+      // error handling
+      console.log("error");
     }
+  };
 
+  const updateImage = (e) => {
+    const file = e.target.files[0];
+    setImage(file);
+  };
 
-    const updateImage = (e) => {
-        const file = e.target.files[0];
-        setImage(file);
+  return (
+    <form onSubmit={handleSubmit}>
+      <input type="file" accept="image/*" onChange={updateImage} />
+      <button type="submit">Submit</button>
+      {imageLoading && <p>Loading...</p>}
+    </form>
+  );
+};
+
+export const UploadSong = () => {
+  const history = useHistory(); // so that we can redirect after the image upload is successful
+  const [song, setSong] = useState(null);
+  const [songLoading, setImageLoading] = useState(false);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const formData = new FormData();
+    formData.append("song", song);
+
+    // aws uploads can be a bit slow—displaying
+    // some sort of loading message is a good idea
+    setImageLoading(true);
+
+    const res = await fetch("/api/song", {
+      method: "POST",
+      body: formData,
+    });
+    if (res.ok) {
+      await res.json();
+      setImageLoading(false);
+      history.push("/song");
+    } else {
+      setImageLoading(false);
+      // a real app would probably use more advanced
+      // error handling
+      console.log("error");
     }
+  };
 
-    return (
-        <form onSubmit={handleSubmit}>
-            <input
-              type="file"
-              accept="image/*"
-              onChange={updateImage}
-            />
-            <button type="submit">Submit</button>
-            {(imageLoading)&& <p>Loading...</p>}
-        </form>
-    )
-}
-
-export default UploadPicture;
+  return (
+    <form onSubmit={handleSubmit}>
+      <input type="file" accept="song/*" />
+      <button type="submit">Upload</button>
+      {songLoading && <p>Loading...</p>}
+    </form>
+  );
+};
