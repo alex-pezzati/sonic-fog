66756e17e06a5c2cff2144a274ac8a064fd963f0
alex-pezzati <aapezzati@gmail.com>
2021-03-27 16:18:48 -0400

    fixed seeding functions and files to properly link to aws resources

--- app/seeds/artists_default.py
+++ app/seeds/artists_default.py
@@ -5,14 +5,20 @@ from werkzeug.security import generate_password_hash
 from app.models import db, User
 
 
+# https://sonicfog.s3.amazonaws.com/seed/banner-images/Childish+Gambino+banner1240x260.jpg
+
 S3_BUCKET = os.environ.get('S3_BUCKET') # get env secret info
-s3_banner = f's3://{S3_BUCKET}/banner-images/' # default aws url for banner images
-s3_profile = f's3://{S3_BUCKET}/profile-images/' # default aws url for profile images
+s3_banner = f'https://{S3_BUCKET}.s3.amazonaws.com/seed/banner-images/' # default aws url for banner images
+s3_profile = f'https://{S3_BUCKET}s3.amazonaws.com/seed/profile-images/' # default aws url for profile images
 artist_data = 'app/seeds/data/artists_data.json' # seed data file location
 
-# converting hyphens and spaces to underscores
-def convert(x):
+
+def convert_snake(x):
     return x.replace(' - ', '_').replace(' ', '_')
+
+
+def convert_url(x):
+    return x.replace(' ', '+')
 
 
 def seed_artists():
@@ -20,14 +26,15 @@ def seed_artists():
         artists_list = json.loads(f.read())
 
         for artist in artists_list:
-            user = convert(artist)
+            user = convert_snake(artist)
+            url = convert_url(artist)
 
             seed = User(
                 display_name=f'{artist}',
                 email=f'{user}@sonicfog.com',
                 hashed_password=generate_password_hash(f'{user}'),
-                profile_url=f'{s3_profile}{user}_profile_500x500.jpg',
-                banner_url=f'{s3_banner}{user}_banner_1240x260.jpg'
+                profile_url=f'{s3_profile}{url}+profile200x200.jpg',
+                banner_url=f'{s3_banner}{url}+banner1240x260.jpg'
             )
             db.session.add(seed)
 
--- app/seeds/songs_default.py
+++ app/seeds/songs_default.py
@@ -2,10 +2,11 @@ import json
 import os
 
 from app.models import db, Song, User
-
-
+# https://sonicfog.s3.amazonaws.com/seed/songs/album+art/Barretso+-+Celeste+album500x500.jpg
+# https://sonicfog.s3.amazonaws.com/seed/songs/Barretso+-+Celeste.mp3
 S3_BUCKET = os.environ.get('S3_BUCKET') # get env secret info
-s3_songs = f's3://{S3_BUCKET}/songs/' # default aws url for songs
+s3_songs = f'https://{S3_BUCKET}.s3.amazonaws.com/seed/songs/' # default aws url for songs
+s3_art = f'https://{S3_BUCKET}.s3.amazonaws.com/seed/songs/album+art/' # default aws url for songs
 songs_data = 'app/seeds/data/songs_data.json' # seed data file location
 
 # trimming song name from file to get artist name
@@ -13,26 +14,30 @@ def artist(x):
     artist, _ = x.split(' -')
     return artist
 
-# converting hyphens and spaces to underscores
-def convert(x):
+
+def convert_snake(x):
     return x.replace(' - ', '_').replace(' ', '_')
 
 
+def convert_url(x):
+    return x.replace(' ', '+')
+# https://sonicfog.s3.amazonaws.com/seed/songs/Barretso+-+Celeste.mp3
+# https://sonicfog.s3.amazonaws.com/seed/songs/album+art/Barretso+-+Celeste+album500x500.jpg
 def seed_songs():
     with open(f'{songs_data}', 'r') as f:
         song_list = json.loads(f.read())
 
         for song in song_list:
             user = artist(song['title'])
-            art = convert(song['title'])
+            url = convert_url(song['title'])
             title = song['title']
 
             seed = Song(
                 user_id = db.session.query(User.id).filter(User.display_name == user).first(),
                 artist = user,
                 name = title,
-                url = f'{s3_songs}{title}',
-                cover_image = f'{s3_songs}{title}{art}_500x500.jpg',
+                url = f'{s3_songs}{url}.mp3',
+                cover_image = f'{s3_art}{url}+album500x500.jpg',
                 duration = song['duration'],
                 normalized_data = song['waveform_data'],
             )
