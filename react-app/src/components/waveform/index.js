a10077a3e38a956501c0a218e940e7a40fcc86ab
Jamie Kichuk <jckichuk@gmail.com>
2021-04-26 20:27:29 -0500

    added notes to waveform

--- react-app/src/components/waveform/index.js
+++ react-app/src/components/waveform/index.js
@@ -5,30 +5,35 @@ import { setCheckpoint, setActiveSongData } from "../../store/song";
 // import classes from './Waveform.module.css'
 
 const Waveform = ({ songId, canvasWidth, canvasHeight }) => {
+  // This is the amplitude data, the array of normalized numbers
   const [waveformData, setWaveFormData] = useState();
-  const [trackDuration, setTrackDuration] = useState();
-  const [songUrl, setSongUrl] = useState();
-
+  // This is the number of filled in bars. Like, how complete the song is, as represented by the number of bars
   const [numWaveformBars, setNumWaveformBars] = useState(-1);
+  // This is the number of USER highlighted bars (which appear when a user tries to seek a track)
   const [numHighlightedBars, setNumHighlightedBars] = useState(-1);
+
   const [targetTime, setTargetTime] = useState(0);
+  const [songUrl, setSongUrl] = useState();
+  const [trackDuration, setTrackDuration] = useState();
 
   const storeSongData = useSelector((store) => store.song);
 
   const dispatch = useDispatch();
 
-  // const { songId, localCurrentTime, waveformData, trackDuration, songUrl } = propSongData
-  // console.log(songUrl, propSongData)
-
   let canvasRef = useRef();
 
 
-  // Setting the initial data
+  // Grab the initial data from the backend
+  //  - the waveform array
+  //  - the duration
+  //  - the url
   useEffect(() => {
     (async () => {
       let response = await fetch(`/api/songs/${songId}`);
       let data = await response.json();
 
+      // The waveform data is stored as a string in the backend, so we have to do some string manipulation
+      // to turn it into a the array of numbers we need
       let waveform_data = data.waveform_data;
       waveform_data = waveform_data.slice(1, -1);
       let arr = waveform_data.split(",");
@@ -42,14 +47,44 @@ const Waveform = ({ songId, canvasWidth, canvasHeight }) => {
     })();
   }, [songId]);
 
+
+
   // Set the number of waveform bars
   useEffect(() => {
+    // If the active song is NOT this song, then reset the number of waveformbars
     if (storeSongData.activeSongId && storeSongData.activeSongId !== songId) {
       setNumWaveformBars(-1);
       return;
     }
 
+    // If there is no waveform data, stop right here
     if ((!waveformData || !waveformData.length) && trackDuration !== 0) return;
+
+
+    // Case1: Start playing, don't stop
+    //  get the message to start from the store
+    //  save the time you got the message
+    //  calculate difference between NOW and the start time
+    //  convert that to seconds, calculate the trackpercentage and proceed as normal
+    // Case2: Start playing, pause, then start
+    //  same as above, but when you get the message to stop you save the current percentage
+    //  when you get the message to resume, you set that as the new start time
+    //  then when you calculate the percentage, you add that to the saved percentage
+    // Case3: Seek the track
+    //  update the track to the checkpoint time. calculate the current percentage. Save that
+    //  get the current time as the start time.
+    //  calculate the percentage as normal, add it to the saved percentage
+
+    // In all cases, if you are starting, resuming or seeking, get the checkpoint/track time, convert it to track percentage and save it
+    // also save the TIME when you saved that percentage and use it to calulcate the difference between right now and then
+    // convert that diff to a percentage, then add that percentage to the saved percentage
+
+    // Pausing: just stop.
+    // Resuming/Playing: get the checkpoint from the store, convert it to track percentage, save it. Also save the time you got it
+    //    then calculate the diffs, convert to percents and add to the saved
+    // Seeking: same as resume/playing (maybe check if the song is already playing or not tho)
+    const current_time = Date.now()
+    console.log(current_time)
 
     requestAnimationFrame(() => {
       const trackPercentage = storeSongData.currentTime / trackDuration;
@@ -69,6 +104,8 @@ const Waveform = ({ songId, canvasWidth, canvasHeight }) => {
       }
     });
   }, [storeSongData, numWaveformBars, songId, trackDuration, waveformData]);
+
+
 
   // Paint the canvas with the waveformbars and the highlighted bars
   useEffect(() => {
