4277ec9ff718bc85f6c55e384dfe6b94f0293e15
alex-pezzati <aapezzati@gmail.com>
2021-05-05 16:47:43 -0400

    removed position styling from global styles; renamed and moved creators component to DevInfo

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,63 +1,63 @@
-import React, { useState, useEffect } from "react";
-import { BrowserRouter, Route, Switch } from "react-router-dom";
-import { useDispatch } from "react-redux";
+import React, { useState, useEffect } from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { useDispatch } from 'react-redux';
 
-import LandingPage from "./components/LandingPage";
-import ProtectedRoute from "./components/auth/ProtectedRoute";
-import NavBar from "./components/upper_navbar/NavBar";
-import SongPageRoute from "./components/songPage";
-import SongNavBar from "./components/song_navbar";
-import UploadPicture from "./services/AWS";
-import UploadSong from "./components/song_upload_form/AWS_Song";
-import User from "./components/user_profile/User";
+import DevInfo from './components/DevInfo';
+import LandingPage from './components/LandingPage';
+import NavBar from './components/upper_navbar/NavBar';
+import ProtectedRoute from './components/auth/ProtectedRoute';
+import SongPageRoute from './components/songPage';
+import SongNavBar from './components/song_navbar';
+import UploadPicture from './services/AWS';
+import UploadSong from './components/song_upload_form/AWS_Song';
+import User from './components/user_profile/User';
 // import WaveFormControls from "./components/waveformControls";
 // import Waveform from "./components/waveform";
 
-import { restoreSession } from "./store/session";
-import Creators from "./components/upper_navbar/creators";
+import { restoreSession } from './store/session';
 // import { authenticate } from "./services/auth";
 
 function App() {
-  const dispatch = useDispatch();
+    const dispatch = useDispatch();
 
-  const [isLoaded, setIsLoaded] = useState(false);
-  // const [authorized, setAuthorized] = useState(false);
+    const [isLoaded, setIsLoaded] = useState(false);
+    // const [authorized, setAuthorized] = useState(false);
 
-  // Try to restore the user
-  useEffect(() => {
-    (async () => {
-      dispatch(restoreSession());
-      setIsLoaded(true);
-    })();
-  }, [dispatch]);
+    // Try to restore the user
+    useEffect(() => {
+        (async () => {
+            dispatch(restoreSession());
+            setIsLoaded(true);
+        })();
+    }, [dispatch]);
 
-  return (
-    isLoaded && (
-      <BrowserRouter>
-        <Route path="/creators" exact>
-          <Creators />
-        </Route>
-        <NavBar isLoaded={isLoaded} />
-        <SongNavBar />
-        {/* <MemoizedSongNavBar/> */}
-        <Switch>
-          <Route path="/" exact={true}>
-            <LandingPage />
-          </Route>
-          <Route path="/songs/:songId">
-            <SongPageRoute />
-          </Route>
-          <ProtectedRoute path="/users/:username" exact={true}>
-            <User />
-          </ProtectedRoute>
-          <ProtectedRoute path="/upload">
-            <UploadSong />
-            <UploadPicture />
-          </ProtectedRoute>
-        </Switch>
-      </BrowserRouter>
-    )
-  );
+    return (
+        isLoaded && (
+            <BrowserRouter>
+                <SongNavBar />
+                {/* <MemoizedSongNavBar/> */}
+                <Switch>
+                    <Route path="/" exact={true}>
+                        <LandingPage />
+                    </Route>
+                    <NavBar isLoaded={isLoaded} />
+                    <Route path="/devs" exact>
+                        <DevInfo />
+                    </Route>
+                    <Route path="/songs/:songId">
+                        <SongPageRoute />
+                    </Route>
+                    <ProtectedRoute path="/upload">
+                        <UploadSong />
+                        <UploadPicture />
+                    </ProtectedRoute>
+                    <ProtectedRoute path="/users/:username" exact={true}>
+                        <User />
+                    </ProtectedRoute>
+                </Switch>
+            </BrowserRouter>
+        )
+    );
 }
 
 export default App;
