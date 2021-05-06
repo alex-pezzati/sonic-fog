0daea1c0ba95f975afd5d05954456f45457616c1
alex-pezzati <aapezzati@gmail.com>
2021-05-06 18:45:04 -0400

    spacing changes

--- react-app/src/components/auth/SignupModal/index.js
+++ react-app/src/components/auth/SignupModal/index.js
@@ -11,149 +11,157 @@ import close from '../../../images/close.svg';
 Modal.setAppElement('#root');
 
 function SignupFormModal() {
-  const dispatch = useDispatch();
-  const history = useHistory();
-  const modalSignUpState = useSelector((state) => state.modal.signup);
+    const dispatch = useDispatch();
+    const history = useHistory();
+    const modalSignUpState = useSelector((state) => state.modal.signup);
 
-  //TODO: Add inputs for firstname, lastname
-  const [email, setEmail] = useState('');
-  const [username, setUsername] = useState('');
-  const [password, setPassword] = useState('');
-  const [confirmPassword, setConfirmPassword] = useState('');
+    //TODO: Add inputs for firstname, lastname
+    const [email, setEmail] = useState('');
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+    const [confirmPassword, setConfirmPassword] = useState('');
 
-  const [errors, setErrors] = useState([]);
+    const [errors, setErrors] = useState([]);
 
-  const closeSignUp = () => {
-    dispatch(modalSignUpClose());
-  };
+    const closeSignUp = () => {
+        dispatch(modalSignUpClose());
+    };
 
-  const closeSignUpOpenLogIn = () => {
-    dispatch(modalSignUpClose());
-    dispatch(modalLogInOpen());
-  };
+    const closeSignUpOpenLogIn = () => {
+        dispatch(modalSignUpClose());
+        dispatch(modalLogInOpen());
+    };
 
-  const handleSubmit = async (e) => {
-    console.log('hi')
-    e.preventDefault();
-    setErrors([]);
-    if (password === confirmPassword) {
-      const user = await dispatch(signup({ username, email, password }));
-      if (!user.errors) {
-        history.push(`/`)
-      } else {
-        setErrors(user.errors);
-      }
-    } else {
-      setErrors(['Passwords must match'])
-    }
-  };
+    const handleSubmit = async (e) => {
+        console.log('hi');
+        e.preventDefault();
+        setErrors([]);
+        if (password === confirmPassword) {
+            const user = await dispatch(signup({ username, email, password }));
+            if (!user.errors) {
+                history.push(`/`);
+            } else {
+                setErrors(user.errors);
+            }
+        } else {
+            setErrors(['Passwords must match']);
+        }
+    };
 
-  let errorRender;
-  if (errors.length > 0) {
-    errorRender = (
-      <div className={c.div}>
-        <ul style={{ color: 'red' }}>
-          {errors && errors.map((error, idx) => <li key={idx}>{error}</li>)}
-        </ul>
-      </div>
-    );
-  }
-
-  const demoLogin = () => {
-    setEmail('demo@aa.io');
-    setPassword('password');
-  };
-  const handleSubmitDemoLogin = async (e) => {
-    e.preventDefault();
-    let res = await dispatch(login({ email, password }))
-    console.log('submit', res)
-    dispatch(modalSignUpClose())
-    history.push(`/`)
-  };
-
-  return (
-    <Modal
-      isOpen={modalSignUpState}
-      className={c.content}
-      overlayClassName={c.overlay}
-      shouldCloseOnOverlayClick={false}
-      shouldFocusAfterRender={true}
-    >
-      <div className={c.container}>
-        <div className={c.x__container}>
-          <button onClick={closeSignUp} className={c.x__button}>
-            <div className={c.x__div}>
-              <img className={c.x__graphic} src={close} alt='close signup' />
+    let errorRender;
+    if (errors.length > 0) {
+        errorRender = (
+            <div className={c.div}>
+                <ul style={{ color: 'red' }}>
+                    {errors &&
+                        errors.map((error, idx) => <li key={idx}>{error}</li>)}
+                </ul>
             </div>
-          </button>
-        </div>
-        {/* LOGO */}
-        <h3 className={c.title}>Welcome to Sonic Fog</h3>
-        <h3 className={c.subtitle}>Noisy and Moist</h3>
-        <div className={c.form__container}>
-          <form onSubmit={handleSubmit} className={c.form}>
-            {errorRender}
-            <input
-              type='text'
-              className={c.input}
-              onChange={(e) => setEmail(e.target.value)}
-              value={email}
-              placeholder='Email'
-              required
-            />
-            <input
-              type='text'
-              className={c.input}
-              onChange={(e) => setUsername(e.target.value)}
-              value={username}
-              placeholder='Username'
-              name='username'
-              required
-            />
-            <input
-              type='password'
-              className={c.input}
-              onChange={(e) => setPassword(e.target.value)}
-              value={password}
-              placeholder='Password'
-              name='password'
-              required
-            />
-            <input
-              type='password'
-              className={c.input}
-              onChange={(e) => setConfirmPassword(e.target.value)}
-              value={confirmPassword}
-              placeholder='Confirm Password'
-              name='confirm password'
-              required
-            />
-            <button type='submit' className={c.continue__button}>
-              Continue
-						</button>
-          </form>
-          <p className={c.or}>OR</p>
-        </div>
-        <form onSubmit={handleSubmitDemoLogin} className={c.form}>
-          <div className={c.div}>
-            <button
-              type='submit'
-              onClick={demoLogin}
-              className={c.demo}
-            >
-              Continue as Demo
-            </button>
-          </div>
-        </form>
-        <div className={c.div__line}></div>
-        <div className={c.div}>
-          <div onClick={(e) => closeSignUpOpenLogIn()} className={c.signup}>
-            Already a member? Log in
-					</div>
-        </div>
-      </div>
-    </Modal>
-  );
+        );
+    }
+
+    const demoLogin = () => {
+        setEmail('demo@aa.io');
+        setPassword('password');
+    };
+    const handleSubmitDemoLogin = async (e) => {
+        e.preventDefault();
+        let res = await dispatch(login({ email, password }));
+        console.log('submit', res);
+        dispatch(modalSignUpClose());
+        history.push(`/`);
+    };
+
+    return (
+        <Modal
+            isOpen={modalSignUpState}
+            className={c.content}
+            overlayClassName={c.overlay}
+            shouldCloseOnOverlayClick={false}
+            shouldFocusAfterRender={true}
+        >
+            <div className={c.container}>
+                <div className={c.x__container}>
+                    <button onClick={closeSignUp} className={c.x__button}>
+                        <div className={c.x__div}>
+                            <img
+                                className={c.x__graphic}
+                                src={close}
+                                alt="close signup"
+                            />
+                        </div>
+                    </button>
+                </div>
+                {/* LOGO */}
+                <h3 className={c.title}>Welcome to Sonic Fog</h3>
+                <h3 className={c.subtitle}>Noisy and Moist</h3>
+                <div className={c.form__container}>
+                    <form onSubmit={handleSubmit} className={c.form}>
+                        {errorRender}
+                        <input
+                            type="text"
+                            className={c.input}
+                            onChange={(e) => setEmail(e.target.value)}
+                            value={email}
+                            placeholder="Email"
+                            required
+                        />
+                        <input
+                            type="text"
+                            className={c.input}
+                            onChange={(e) => setUsername(e.target.value)}
+                            value={username}
+                            placeholder="Username"
+                            name="username"
+                            required
+                        />
+                        <input
+                            type="password"
+                            className={c.input}
+                            onChange={(e) => setPassword(e.target.value)}
+                            value={password}
+                            placeholder="Password"
+                            name="password"
+                            required
+                        />
+                        <input
+                            type="password"
+                            className={c.input}
+                            onChange={(e) => setConfirmPassword(e.target.value)}
+                            value={confirmPassword}
+                            placeholder="Confirm Password"
+                            name="confirm password"
+                            required
+                        />
+                        <button type="submit" className={c.continue__button}>
+                            Continue
+                        </button>
+                    </form>
+                    <p className={c.or}>OR</p>
+                </div>
+                <form onSubmit={handleSubmitDemoLogin} className={c.form}>
+                    <div className={c.div}>
+                        <button
+                            type="submit"
+                            onClick={demoLogin}
+                            className={c.demo}
+                        >
+                            Continue as Demo
+                        </button>
+                    </div>
+                </form>
+                <div className={c.div__line}></div>
+                <div className={c.div}>
+                    <div
+                        onClick={(e) => closeSignUpOpenLogIn()}
+                        className={c.signup}
+                    >
+                        Already a member? Log in
+                    </div>
+                </div>
+            </div>
+        </Modal>
+    );
 }
 
 export default SignupFormModal;
--- react-app/src/store/song.js
+++ react-app/src/store/song.js
@@ -1,84 +1,84 @@
 let counter = 0;
 
-const SET_ACTIVE_SONG_DATA = "song/SET_ACTIVE_SONG";
-const SET_CHECKPOINT = "song/SET_CHECKPOINT";
-const PAUSE_SONG = "song/PAUSE_SONG";
-const PLAY_SONG = "song/PLAY_SONG";
+const SET_ACTIVE_SONG_DATA = 'song/SET_ACTIVE_SONG';
+const SET_CHECKPOINT = 'song/SET_CHECKPOINT';
+const PAUSE_SONG = 'song/PAUSE_SONG';
+const PLAY_SONG = 'song/PLAY_SONG';
 
-const SET_AUDIO_REF = "song/SET_AUDIO_REF";
+const SET_AUDIO_REF = 'song/SET_AUDIO_REF';
 
 export const pauseSong = () => {
-  return {
-    type: PAUSE_SONG,
-  };
+    return {
+        type: PAUSE_SONG,
+    };
 };
 
 export const playSong = () => {
-  return {
-    type: PLAY_SONG,
-  };
+    return {
+        type: PLAY_SONG,
+    };
 };
 
 export const setActiveSongData = (songId, songURL) => {
-  return {
-    type: SET_ACTIVE_SONG_DATA,
-    data: { songId, songURL },
-  };
+    return {
+        type: SET_ACTIVE_SONG_DATA,
+        data: { songId, songURL },
+    };
 };
 
 export const setCheckpoint = (seconds) => {
-  if (counter % 2) counter++;
-  else counter--;
+    if (counter % 2) counter++;
+    else counter--;
 
-  return {
-    type: SET_CHECKPOINT,
-    seconds: seconds + counter * 0.01,
-  };
+    return {
+        type: SET_CHECKPOINT,
+        seconds: seconds + counter * 0.01,
+    };
 };
 
 export const setAudioRef = (audioRef) => {
-  return {
-    type: SET_AUDIO_REF,
-    audioRef,
-  };
+    return {
+        type: SET_AUDIO_REF,
+        audioRef,
+    };
 };
 
 const initialStore = {
-  checkpoint: 0,
-  activeSongId: null,
-  activeSongURL: null,
-  isPlaying: false,
-  audioRef: null,
+    checkpoint: 0,
+    activeSongId: null,
+    activeSongURL: null,
+    isPlaying: false,
+    audioRef: null,
 };
 
 const songReducer = (songData = initialStore, action) => {
-  let newData;
-  switch (action.type) {
-    case SET_CHECKPOINT:
-      newData = { ...songData };
-      newData["checkpoint"] = action.seconds;
-      return newData;
-    case SET_ACTIVE_SONG_DATA:
-      newData = { ...songData };
-      newData["activeSongId"] = action.data.songId;
-      newData["activeSongURL"] = action.data.songURL;
-      newData["checkpoint"] = 0.0;
-      return newData;
-    case PAUSE_SONG:
-      newData = { ...songData };
-      newData["isPlaying"] = false;
-      return newData;
-    case PLAY_SONG:
-      newData = { ...songData };
-      newData["isPlaying"] = true;
-      return newData;
-    case SET_AUDIO_REF:
-      newData = { ...songData };
-      newData["audioRef"] = action.audioRef;
-      return newData;
-    default:
-      return songData;
-  }
+    let newData;
+    switch (action.type) {
+        case SET_CHECKPOINT:
+            newData = { ...songData };
+            newData['checkpoint'] = action.seconds;
+            return newData;
+        case SET_ACTIVE_SONG_DATA:
+            newData = { ...songData };
+            newData['activeSongId'] = action.data.songId;
+            newData['activeSongURL'] = action.data.songURL;
+            newData['checkpoint'] = 0.0;
+            return newData;
+        case PAUSE_SONG:
+            newData = { ...songData };
+            newData['isPlaying'] = false;
+            return newData;
+        case PLAY_SONG:
+            newData = { ...songData };
+            newData['isPlaying'] = true;
+            return newData;
+        case SET_AUDIO_REF:
+            newData = { ...songData };
+            newData['audioRef'] = action.audioRef;
+            return newData;
+        default:
+            return songData;
+    }
 };
 
 export default songReducer;
