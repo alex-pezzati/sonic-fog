f33394e6aac159fa3a9b9d898227c1e342461dd7
alex-pezzati <aapezzati@gmail.com>
2021-05-06 19:19:46 -0400

    fixed isloaded prop threading bug for navbar

--- react-app/src/components/MainNavBar/index.js
+++ react-app/src/components/MainNavBar/index.js
@@ -2,6 +2,6 @@ import React from 'react';
 
 import NavBar from './NavBar';
 
-const MainNavBar = () => <NavBar />;
+const MainNavBar = ({ isLoaded }) => <NavBar isLoaded={isLoaded} />;
 
 export default MainNavBar;
