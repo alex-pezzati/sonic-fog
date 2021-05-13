eaaf2e949da3fb3bf3756fcc5721c26028374702
alex-pezzati <aapezzati@gmail.com>
2021-05-13 02:04:27 -0400

    last edits to devcard and signin and createaccount modals

--- react-app/src/components/DevInfo/DevCard.js
+++ react-app/src/components/DevInfo/DevCard.js
@@ -3,6 +3,11 @@ import React from 'react';
 import c from './DevCard.module.css';
 
 const DevCard = ({ dev }) => {
+    /* TODO:
+        -- settle on proper icons (svg) for hover effect to work
+        --
+    */
+
     return (
         <div className={c.container}>
             <div className={c.picture__container}>
@@ -13,6 +18,7 @@ const DevCard = ({ dev }) => {
                 ></img>
             </div>
             <h3 className={c.name}>{dev.NAME}</h3>
+            <div className={c.line}></div>
             <div className={c.links__container}>
                 <a href={dev.GITHUB} rel="noopener noreferrer" target="_blank">
                     <div className={c.github}></div>
