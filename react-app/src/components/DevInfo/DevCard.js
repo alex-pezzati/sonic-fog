19e2fc956484d27862b7e4d4576354790f01e298
alex-pezzati <aapezzati@gmail.com>
2021-05-12 22:11:56 -0400

    initial refactor of devinfo; added constants

---
+++ react-app/src/components/DevInfo/DevCard.js
@@ -0,0 +1,112 @@
+import React from 'react';
+
+import c from './DevCard.module.css';
+
+const DevCard = (dev) => {
+    console.log(dev);
+    return <div></div>;
+    // <div className={classes.parent}>
+    //     <div className={classes.outer}>
+    //         <fieldset className={(classes.fieldset, classes.Alex)}>
+    //             <legend className={classes.legend}>Alex Pezzati</legend>
+    //             <ul>
+    //                 <li>
+    //                     <a
+    //                         rel="noopener noreferrer"
+    //                         target="_blank"
+    //                         href="https://github.com/alex-pezzati"
+    //                     >
+    //                         GitHub
+    //                     </a>
+    //                 </li>
+    //                 <li>
+    //                     <a
+    //                         rel="noopener noreferrer"
+    //                         target="_blank"
+    //                         href="https://www.linkedin.com/"
+    //                     >
+    //                         LinkedIn
+    //                     </a>
+    //                 </li>
+    //                 <li>
+    //                     <a
+    //                         rel="noopener noreferrer"
+    //                         target="_blank"
+    //                         href="https://www.google.com"
+    //                     >
+    //                         Personal Site
+    //                     </a>
+    //                 </li>
+    //             </ul>
+    //         </fieldset>
+    //         <fieldset className={(classes.fieldset, classes.Jamie)}>
+    //             <legend className={classes.legend}>Jamie Kichuk</legend>
+    //             <ul>
+    //                 <li>
+    //                     <a
+    //                         rel="noopener noreferrer"
+    //                         target="_blank"
+    //                         href="https://github.com/JKLolling"
+    //                     >
+    //                         GitHub
+    //                     </a>
+    //                 </li>
+    //                 <li>
+    //                     <a
+    //                         rel="noopener noreferrer"
+    //                         target="_blank"
+    //                         href="https://www.linkedin.com/in/jamie-kichuk-45778068/"
+    //                     >
+    //                         LinkedIn
+    //                     </a>
+    //                 </li>
+    //                 <li>
+    //                     <a
+    //                         rel="noopener noreferrer"
+    //                         target="_blank"
+    //                         href="https://www.jkichuk.com"
+    //                     >
+    //                         Personal Site
+    //                     </a>
+    //                 </li>
+    //             </ul>
+    //         </fieldset>
+    //         <fieldset className={(classes.fieldset, classes.Raymond)}>
+    //             <legend className={classes.legend}>
+    //                 Raymond-Arthur May
+    //             </legend>
+    //             <ul>
+    //                 <li>
+    //                     <a
+    //                         rel="noopener noreferrer"
+    //                         target="_blank"
+    //                         href="https://github.com/raymondmay95"
+    //                     >
+    //                         GitHub
+    //                     </a>
+    //                 </li>
+    //                 <li>
+    //                     <a
+    //                         rel="noopener noreferrer"
+    //                         target="_blank"
+    //                         href="https://www.linkedin.com/in/coderay/"
+    //                     >
+    //                         LinkedIn
+    //                     </a>
+    //                 </li>
+    //                 <li>
+    //                     <a
+    //                         rel="noopener noreferrer"
+    //                         target="_blank"
+    //                         href="https://www.raymondmay.com"
+    //                     >
+    //                         Personal Site
+    //                     </a>
+    //                 </li>
+    //             </ul>
+    //         </fieldset>
+    //     </div>
+    // </div>
+};
+
+export default DevCard;
--- react-app/src/components/DevInfo/index.js
+++ react-app/src/components/DevInfo/index.js
@@ -1,141 +1,33 @@
-import React from 'react'
-import * as classes from './DevInfo.module.css';
+import React from 'react';
+
+import DevCard from './DevCard';
 
 function DevInfo() {
-    return (
-        <div className={classes.parent}>
-            <div className={classes.outer}>
-                <fieldset className={(classes.fieldset, classes.Alex)}>
-                    <legend className={classes.legend}>Alex Pezzati</legend>
-                    <ul>
-                        <li>
-                            <a
-                                rel="noopener noreferrer"
-                                target="_blank"
-                                href="https://github.com/alex-pezzati"
-                            >
-                                GitHub
-                            </a>
-                        </li>
-                        <li>
-                            <a
-                                rel="noopener noreferrer"
-                                target="_blank"
-                                href="https://www.linkedin.com/"
-                            >
-                                LinkedIn
-                            </a>
-                        </li>
-                        <li>
-                            <a
-                                rel="noopener noreferrer"
-                                target="_blank"
-                                href="https://www.google.com"
-                            >
-                                Personal Site
-                            </a>
-                        </li>
-                    </ul>
-                </fieldset>
-                <fieldset className={(classes.fieldset, classes.Chou)}>
-                    <legend className={classes.legend}>Chou Fomenky</legend>
-                    <ul>
-                        <li>
-                            <a
-                                rel="noopener noreferrer"
-                                target="_blank"
-                                href="https://github.com/alex-pezzati"
-                            >
-                                GitHub
-                            </a>
-                        </li>
-                        <li>
-                            <a
-                                rel="noopener noreferrer"
-                                target="_blank"
-                                href="https://www.linkedin.com/in/alex-pezzati/"
-                            >
-                                LinkedIn
-                            </a>
-                        </li>
-                        <li>
-                            <a
-                                rel="noopener noreferrer"
-                                target="_blank"
-                                href="https://www.google.com/"
-                            >
-                                Personal Site
-                            </a>
-                        </li>
-                    </ul>
-                </fieldset>
-                <fieldset className={(classes.fieldset, classes.Jamie)}>
-                    <legend className={classes.legend}>Jamie Kichuk</legend>
-                    <ul>
-                        <li>
-                            <a
-                                rel="noopener noreferrer"
-                                target="_blank"
-                                href="https://github.com/JKLolling"
-                            >
-                                GitHub
-                            </a>
-                        </li>
-                        <li>
-                            <a
-                                rel="noopener noreferrer"
-                                target="_blank"
-                                href="https://www.linkedin.com/in/jamie-kichuk-45778068/"
-                            >
-                                LinkedIn
-                            </a>
-                        </li>
-                        <li>
-                            <a
-                                rel="noopener noreferrer"
-                                target="_blank"
-                                href="https://www.jkichuk.com"
-                            >
-                                Personal Site
-                            </a>
-                        </li>
-                    </ul>
-                </fieldset>
-                <fieldset className={(classes.fieldset, classes.Raymond)}>
-                    <legend className={classes.legend}>Raymond-Arthur May</legend>
-                    <ul>
-                        <li>
-                            <a
-                                rel="noopener noreferrer"
-                                target="_blank"
-                                href="https://github.com/raymondmay95"
-                            >
-                                GitHub
-                            </a>
-                        </li>
-                        <li>
-                            <a
-                                rel="noopener noreferrer"
-                                target="_blank"
-                                href="https://www.linkedin.com/in/coderay/"
-                            >
-                                LinkedIn
-                            </a>
-                        </li>
-                        <li>
-                            <a
-                                rel="noopener noreferrer"
-                                target="_blank"
-                                href="https://www.raymondmay.com"
-                            >
-                                Personal Site
-                            </a>
-                        </li>
-                    </ul>
-                </fieldset>
-            </div>
-        </div>
-    );
+    // dev info constants
+    const RAYMOND = {
+        NAME: 'Raymond-Arthur May',
+        GITHUB: 'https://github.com/raymondmay95',
+        LINKEDIN: 'https://www.linkedin.com/in/coderay//',
+        PORTFOLIO: 'https://www.raymondmay.com',
+    };
+
+    const JAMIE = {
+        NAME: 'Jamie Kichuk',
+        GITHUB: 'https://github.com/JKLolling"',
+        LINKEDIN: 'https://www.linkedin.com/in/jamie-kichuk-45778068/',
+        PORTFOLIO: 'https://www.jkichuk.com',
+    };
+
+    const ALEX = {
+        NAME: 'Alex Pezzati',
+        GITHUB: 'https://github.com/alex-pezzati',
+        LINKEDIN: 'https://www.linkedin.com/in/alex-pezzati/',
+        PORTFOLIO: 'https://github.com/alex-pezzati',
+    };
+
+    const devs = [RAYMOND, JAMIE, ALEX];
+
+    return devs.map((dev) => <DevCard dev={dev} key={dev.name} />);
 }
 
 export default DevInfo;
