8af423a4fdd058232307073fbdd213f2ffd8d97e
alex-pezzati <aapezzati@gmail.com>
2021-05-16 17:02:06 -0400

    demo user display_name changed to Demo for site readability

--- app/seeds/users.py
+++ app/seeds/users.py
@@ -1,13 +1,17 @@
 from werkzeug.security import generate_password_hash
 from app.models import db, User
 
+
 # Adds a demo user, you can add other users here if you want
 def seed_users():
 
-    demo = User(display_name='demo', email='demo@aa.io', hashed_password=generate_password_hash('password'))
+    demo = User(display_name='Demo',
+                email='demo@aa.io',
+                hashed_password=generate_password_hash('password'))
 
     db.session.add(demo)
     db.session.commit()
+
 
 # Uses a raw SQL query to TRUNCATE the users table.
 # SQLAlchemy doesn't have a built in function to do this
